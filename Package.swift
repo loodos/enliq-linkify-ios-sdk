65dcc98b51a190bf5cea443c5ada2319e8c52d0f
fatihtufanloodos <fatih.tufan@loodos.com>
2025-08-08 17:22:55 +0300

    Release v0.0.1

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EIQLinkify",
-            url: "https://github.com/loodos/enliq-linkify-ios-sdk/releases/download/1.0.0/eiqlinkify.xcframework.zip",
-            checksum: "a11bf7bd037eec83c15e6ed283d55a8343787b9be19014ea013ad5f720f0b1e8"
+            url: "https://github.com/loodos/enliq-linkify-ios-sdk/releases/download/0.0.1/eiqlinkify.xcframework.zip",
+            checksum: "bcc2a4a5d03951c33dc5adfc11a5cea74af4ea8e27a2919605e7d7041e6bf21e"
         )
     ]
 )
